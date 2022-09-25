313ebeec0db1594ca2daceefc168b2f8ec2156b1
carlaospa <carlaospa@gmail.com>
2022-09-25 15:38:07 -0300

    Atualização

--- 13_ContentNegotiation/rest-with-spring-boot-and-java/src/main/java/com/carlaospa/PersonController.java
+++ 13_ContentNegotiation/rest-with-spring-boot-and-java/src/main/java/com/carlaospa/PersonController.java
@@ -3,7 +3,6 @@ package com.carlaospa;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.carlaospa.data.vo.v1.PersonVO;
 import com.carlaospa.services.PersonServices;
+import com.carlaospa.util.MediaType;
+
 
 @RestController
 @RequestMapping("/api/person/v1")
@@ -25,29 +26,29 @@ public class PersonController {
 	private PersonServices service;
 	
 	@GetMapping( 
-			produces = MediaType.APPLICATION_JSON_VALUE)
+			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
 	public List<PersonVO> findAll() throws Exception {
 		return service.findAll();
 	}
 
 	@GetMapping(value = "/{id}",
-			produces = MediaType.APPLICATION_JSON_VALUE)
+			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YML })
 	public PersonVO findBy(@PathVariable(value = "id") Long id) throws Exception {
 
 		return service.findById(id);
 	}
 	
 	@PostMapping( 
-			consumes = MediaType.APPLICATION_JSON_VALUE,
-			produces = MediaType.APPLICATION_JSON_VALUE)
+			consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML },
+			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
 	public PersonVO create(@RequestBody PersonVO person) throws Exception {
 
 		return service.create(person);
 	}	
 	
 	@PutMapping(
-			consumes = MediaType.APPLICATION_JSON_VALUE,
-			produces = MediaType.APPLICATION_JSON_VALUE)
+			consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML },
+			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
 	public PersonVO update(@RequestBody PersonVO person) throws Exception {
 
 		return service.update(person);
---
+++ 13_ContentNegotiation/rest-with-spring-boot-and-java/src/main/java/com/carlaospa/config/WebConfig.java
@@ -0,0 +1,45 @@
+package com.carlaospa.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.carlaospa.serialization.converter.YamlJackson2HttpMessageConverter;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	
+	private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
+	
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(new YamlJackson2HttpMessageConverter());
+	}	
+	
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        /*
+		//QUERY PARAM
+		configurer.favorParameter(true)
+				.parameterName("mediaType").ignoreAcceptHeader(true)
+				.useRegisteredExtensionsOnly(false)
+				.defaultContentType(MediaType.APPLICATION_JSON)
+					.mediaType("json", MediaType.APPLICATION_JSON)
+					.mediaType("xml", MediaType.APPLICATION_XML); */
+		
+		
+		//HEADER PARAM
+		configurer.favorParameter(false)
+				.ignoreAcceptHeader(false)
+				.useRegisteredExtensionsOnly(false)
+				.defaultContentType(MediaType.APPLICATION_JSON)
+					.mediaType("json", MediaType.APPLICATION_JSON)
+					.mediaType("xml", MediaType.APPLICATION_XML)
+					.mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML);
+	}
+
+}
