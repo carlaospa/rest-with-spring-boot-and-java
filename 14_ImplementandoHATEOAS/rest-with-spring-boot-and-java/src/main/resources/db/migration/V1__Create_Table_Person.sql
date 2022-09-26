b4f33335896a6af30ab695c617fd54293a7eafea
carlaospa <carlaospa@gmail.com>
2022-09-26 10:00:33 -0300

    Atualização

---
+++ 14_ImplementandoHATEOAS/rest-with-spring-boot-and-java/src/main/resources/db/migration/V1__Create_Table_Person.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `person` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `firt_name` varchar(80) NOT NULL,
+  `last_name` varchar(80) NOT NULL,
+  `address` varchar(100) DEFAULT NULL,  
+  `gender` varchar(6) DEFAULT NULL,  
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
---
+++ 15_SuporteAoSwagger/rest-with-spring-boot-and-java/src/main/java/com/carlaospa/PersonController.java
@@ -0,0 +1,64 @@
+package com.carlaospa;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.carlaospa.data.vo.v1.PersonVO;
+import com.carlaospa.services.PersonServices;
+import com.carlaospa.util.MediaType;
+
+
+@RestController
+@RequestMapping("/api/person/v1")
+public class PersonController {
+
+	@Autowired
+	private PersonServices service;
+	
+	@GetMapping( 
+			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
+	public List<PersonVO> findAll() throws Exception {
+		return service.findAll();
+	}
+
+	@GetMapping(value = "/{id}",
+			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YML })
+	public PersonVO findById(@PathVariable(value = "id") Long id) throws Exception {
+
+		return service.findById(id);
+	}
+	
+	@PostMapping( 
+			consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML },
+			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
+	public PersonVO create(@RequestBody PersonVO person) throws Exception {
+
+		return service.create(person);
+	}	
+	
+	@PutMapping(
+			consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML },
+			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
+	public PersonVO update(@RequestBody PersonVO person) throws Exception {
+
+		return service.update(person);
+	}	
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) throws Exception {
+
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+}
