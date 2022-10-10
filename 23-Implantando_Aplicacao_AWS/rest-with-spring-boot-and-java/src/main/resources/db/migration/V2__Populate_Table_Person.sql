8617fe1857ce62f12d28ad2f73b1e9ea99b5215f
carlaospa <carlaospa@gmail.com>
2022-10-10 14:22:29 -0300

    Atualizacao

--- 23-Implantando_Aplicacao_AWS/rest-with-spring-boot-and-java/src/main/resources/db/migration/V2__Populate_Table_Person.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO person (id, address, first_name, gender, last_name) VALUES 
-(1,'Uberlândia','Carlos Alberto','Male','Rodrigues'),
-(2,'Uberlândia','Marluci','Female','Rodrigues'),
-(3,'Uberlândia','Carla','Female',' Rodrigues'),
-(6,'Uberlândia','Vera Lucia','Female','Rodrigues'),
-(7,'Uberlândia','Nelson','Male','Mandela'),
-(8,'Uberlândia','Nikola','Male','Tesla');
